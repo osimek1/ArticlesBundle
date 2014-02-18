1f78e24e51138d57868b79e42a84aa818e9d32e5
Grzegorz Osimowicz <osimek1@gmail.com>
2014-02-18 23:44:16 +0100

    Rest controller for geting article

--- Controller/ArticleController.php
+++ Controller/ArticleController.php
@@ -3,8 +3,8 @@ namespace Osimek1\ArticlesBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
@@ -21,5 +21,20 @@ class ArticleController extends Controller
         $article = $this->get('osimek1.articles.manager')->getArticleByTranslationSlug($slug, true);
         
         return compact('article');
+    }
+    
+    /**
+     * @Method({"GET"})
+     */
+    public function getArticleRestAction($slug)
+    {
+        $article = $this->get('osimek1.articles.manager')->getArticleByTranslationSlug($slug, true);
+        
+        return new JsonResponse(array(
+            'title' => $article->getTitle(),
+            'shortDesc' => $article->getShortDesc(),
+            'slug' => $article->getSlug(),
+            'content' => $article->getArticleContent()
+        ));
     }
 }
